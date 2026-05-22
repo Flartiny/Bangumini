461ec5bd879bee865ae85aa34e888e7e2cfb76ad
Flartiny <1335348298@qq.com>
2026-05-22 17:13:18 +0800

    fix: compile error

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -20,31 +20,26 @@ fn set_autostart(enabled: bool) -> Result<(), String> {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-        .plugin(tauri_plugin_autostart::init())
+        .plugin(tauri_plugin_autostart::init(
+            tauri_plugin_autostart::MacosLauncher::LaunchAgent,
+            Some(&[]),
+        ))
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![get_shortcut, set_autostart])
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
-            // Register default global shortcut
-            #[cfg(desktop)]
-            {
-                use tauri_plugin_global_shortcut::GlobalShortcutExt;
-                let app_handle = app.handle().clone();
-                app_handle
-                    .plugin(
-                        tauri_plugin_global_shortcut::Builder::new()
-                            .with_handler(move |_shortcut, _event| {
-                                if let Ok(true) = window.is_visible() {
-                                    let _ = window.hide();
-                                } else {
-                                    let _ = window.show();
-                                    let _ = window.set_focus();
-                                }
-                            })
-                            .build(),
-                    )
-                    .ok();
-            }
+            app.handle().plugin(
+                tauri_plugin_global_shortcut::Builder::new()
+                    .with_handler(move |_app, _shortcut, _event| {
+                        if let Ok(true) = window.is_visible() {
+                            let _ = window.hide();
+                        } else {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    })
+                    .build(),
+            )?;
             Ok(())
         })
         .run(tauri::generate_context!())
